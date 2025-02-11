50de299f797aa0e80aa519a92ad5306ef4b6d6fe
BernardOnuh <onuhbernard4@gmail.com>
2025-02-11 18:38:13 +0100

    upload-vercel

--- pages/index.js
+++ pages/index.js
@@ -9,15 +9,6 @@ import FAQ from "@/components/Faq";
 import { Geist, Geist_Mono } from "next/font/google";
 import Footer from "@/components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
   return (
